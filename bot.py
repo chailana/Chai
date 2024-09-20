0f9ca78dba393a679aa5063bb949046d40e416bd
chailana <chailana35217793@gmail.com>
2024-09-20 16:56:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import logging
 import os
 import requests
 from moviepy.editor import VideoFileClip
-from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackQueryHandler, ContextTypes, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackQueryHandler, ContextTypes, filters
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from pytube import YouTube
 
