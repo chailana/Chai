734080eff00b740a1ad51c46642d08a24f78b26a
chailana <chailana35217793@gmail.com>
2024-09-21 15:09:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -74,5 +74,11 @@ async def main():
 
 if __name__ == '__main__':
     os.makedirs('downloads', exist_ok=True)
-    loop = asyncio.get_event_loop()
+
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+
     loop.run_until_complete(main())
