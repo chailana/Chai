fe02748b27dd8d119549fc5b378e8a425dc35878
chailana <chailana35217793@gmail.com>
2024-09-21 21:53:44 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,12 @@
-gunicorn app:app python bot.py 
+### start.sh
+bash
+#!/bin/bash
+
+# Start Gunicorn in the background
+gunicorn app:app --bind 0.0.0.0:8000 &
+
+# Start your bot
+python bot.py
+
+# Wait for background processes to finish
+wait
