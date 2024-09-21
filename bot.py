383aef6749a2959693b3d05f5cbf0147a2624a8a
chailana <chailana35217793@gmail.com>
2024-09-21 15:34:00 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import requests
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import yt_dlp
@@ -16,7 +17,7 @@ async def download_and_send(update: Update, context: ContextTypes.DEFAULT_TYPE):
         return
 
     url = context.args[0]
-    title, size, thumbnail = get_video_info(url)
+    title, size, thumbnail_url = get_video_info(url)
 
     # Download the video
     ydl_opts = {
@@ -28,15 +29,26 @@ async def download_and_send(update: Update, context: ContextTypes.DEFAULT_TYPE):
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([url])
 
+        # Download the thumbnail
+        thumbnail_response = requests.get(thumbnail_url)
+        thumbnail_path = f"{title}_thumbnail.jpg"
+        with open(thumbnail_path, 'wb') as f:
+            f.write(thumbnail_response.content)
+
         # Send the video
         await context.bot.send_video(chat_id=update.effective_chat.id, video=open(f"{title}.mp4", 'rb'),
-                                      caption=f'Title: {title}\nSize: {size}\nThumbnail: {thumbnail}')
+                                      caption=f'Title: {title}\nSize: {size}')
+
+        # Send the thumbnail
+        await context.bot.send_photo(chat_id=update.effective_chat.id, photo=open(thumbnail_path, 'rb'))
+
     except Exception as e:
         await update.message.reply_text(f'Error: {str(e)}')
     finally:
-        # Clean up downloaded file
+        # Clean up downloaded files
         try:
             os.remove(f"{title}.mp4")
+            os.remove(thumbnail_path)
         except:
             pass
 
