4c09801902d506a682a754d28df90a3001131b3f
chailana <chailana35217793@gmail.com>
2024-09-23 12:21:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -162,10 +162,10 @@ async def download_video(user_id, url, format_id):
 def is_valid_url(url):
     regex = re.compile(
         r'^(?:http|ftp)s?://'  # http:// or https://
-        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}|[A-Z0-9-]{2,}\.?)|'  # domain...
         r'localhost|'  # localhost...
         r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|'  # IPv4
-        r'?[A-F0-9:\.]+?'  # IPv6
+        r'?[A-F0-9]*:[A-F0-9:]+?)'  # IPv6
         r'(?::\d+)?'  # optional port
         r'(?:/?|[/?]\S+)$', re.IGNORECASE)
     return re.match(regex, url) is not None
